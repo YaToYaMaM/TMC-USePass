c7db59605d60ab8758a2cef6fc5133b220f39d8c
YaToYaMaM <froilancaete19@gmail.com>
2025-07-09 16:12:11 +0800

    Update Incident Report

--- USePass/resources/js/Pages/Frontend/IncidentReportTemplate.vue
+++ USePass/resources/js/Pages/Frontend/IncidentReportTemplate.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { onMounted } from 'vue';
 import { usePage } from '@inertiajs/vue3';
+import {Head} from "@inertiajs/vue3";
 
 const props = defineProps<{
     report: {
@@ -22,14 +23,15 @@ onMounted(() => {
 </script>
 
 <template>
+    <Head title="Print Report"/>
     <div v-if="props.report"  class="p-10 text-sm font-sans leading-relaxed text-black max-w-3xl mx-auto bg-white">
         <h1 class="text-lg font-bold text-center mb-4">University of Southeastern Philippines</h1>
         <h2 class="text-center text-sm">Security Service Unit</h2>
-        <h3 class="text-center font-bold text-lg mt-2 mb-1">Incident Report</h3>
-        <p class="text-right mb-4">Date: {{ report.date }}</p>
+        <h3 class="text-center font-bold text-lg mt-2 mb-1">INCIDENT REPORT</h3>
+        <p class="text-left mb-4 text-base">Date: <span class="underline">{{ report.date }}</span></p>
 
-        <p class="mb-4">TO WHOM IT MAY CONCERN:</p>
-        <p class="mb-4">
+        <p class="mb-4 font-bold">TO WHOM IT MAY CONCERN:</p>
+        <p class="mb-4 mx-5">
             This is to submit my report with regards to the incident reported in my area of responsibility, to wit:
         </p>
 
@@ -47,15 +49,32 @@ onMounted(() => {
             <div class="text-left">
                 <p>Yours truly,</p>
                 <br />
-                <p class="underline">{{ report.name }}</p>
-                <p>(Guard on Duty)</p>
+                <p class="underline text-xl">{{ report.name }}</p>
+                <p>(Signature Over Printed Name)</p>
+                <p><strong>Guard on Duty</strong></p>
             </div>
             <div class="text-right">
                 <p>Information Validated:</p>
                 <br />
-                <p class="underline">_______________________</p>
-                <p>(Complainant)</p>
+                <p class="underline">_________________________</p>
+                <p>(Signature Over Printed Name</p>
+                <p><strong>Complainant</strong></p>
             </div>
+
+        </div>
+        <div class="flex justify-between mt-10">
+            <div class="text-left">
+                <p>Verified: </p>
+                <br />
+                <p class="underline">_________________________</p>
+
+            </div>
+            <div class="text-right">
+                <p>Noted:</p>
+                <br />
+                <p class="underline">_________________________</p>
+            </div>
+
         </div>
     </div>
 </template>
