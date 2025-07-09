2415a0db15a1eebc5f538e1b408c6c51452b9b99
YaToYaMaM <froilancaete19@gmail.com>
2025-07-09 16:18:53 +0800

    Update Incident Report

--- USePass/resources/js/Pages/Frontend/IncidentReportTemplate.vue
+++ USePass/resources/js/Pages/Frontend/IncidentReportTemplate.vue
@@ -25,7 +25,7 @@ onMounted(() => {
 <template>
     <Head title="Print Report"/>
     <div v-if="props.report"  class="p-10 text-sm font-sans leading-relaxed text-black max-w-3xl mx-auto bg-white">
-        <h1 class="text-lg font-bold text-center mb-4">University of Southeastern Philippines</h1>
+        <h1 class="text-lg font-bold text-center mb-1">University of Southeastern Philippines</h1>
         <h2 class="text-center text-sm">Security Service Unit</h2>
         <h3 class="text-center font-bold text-lg mt-2 mb-1">INCIDENT REPORT</h3>
         <p class="text-left mb-4 text-base">Date: <span class="underline">{{ report.date }}</span></p>
@@ -36,13 +36,13 @@ onMounted(() => {
         </p>
 
         <div class="space-y-2">
-            <p><strong>WHAT:</strong> {{ report.what }}</p>
-            <p><strong>WHO:</strong> {{ report.who }}</p>
-            <p><strong>WHERE:</strong> {{ report.where }}</p>
-            <p><strong>WHEN:</strong> {{ report.when }}</p>
-            <p><strong>HOW:</strong> {{ report.how }}</p>
-            <p><strong>WHY:</strong> {{ report.why }}</p>
-            <p><strong>RECOMMENDATION:</strong> {{ report.recommendation }}</p>
+            <p><strong>WHAT:</strong><span class="underline px-5 text-wrap">{{ report.what }}</span></p>
+            <p><strong>WHO:</strong><span class="underline px-5 text-wrap"> {{ report.who }}</span></p>
+            <p><strong>WHERE:</strong><span class="underline px-5 text-wrap"> {{ report.where }}</span></p>
+            <p><strong>WHEN:</strong><span class="underline px-5 text-wrap"> {{ report.when }}</span></p>
+            <p><strong>HOW:</strong><span class="underline px-5 text-wrap"> {{ report.how }}</span></p>
+            <p><strong>WHY:</strong><span class="underline px-5 text-wrap"> {{ report.why }}</span></p>
+            <p><strong>RECOMMENDATION:</strong><span class="underline px-5 text-wrap"> {{ report.recommendation }}</span></p>
         </div>
 
         <div class="flex justify-between mt-10">
