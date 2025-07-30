996dfc43f82d0d74b14e4b910547d9a87780a29d
YaToYaMaM <froilancaete19@gmail.com>
2025-07-30 14:24:27 +0800

    update student

--- USePass/app/Http/Controllers/StudentController.php
+++ USePass/app/Http/Controllers/StudentController.php
@@ -151,7 +151,7 @@ class StudentController extends Controller
     {
         $categories = [
             'BSED' => ['Special Needs Education', 'Elementary Education', 'Early Childhood Education','English', 'Mathematics', 'Filipino'],
-            'BSIT' => ['Information Technology'],
+            'BSIT' => ['Information Security'],
             'BSABE' => ['Land and Water Resources', 'Machinery and Power', 'Process Engineering','Structure and Environment'],
             'BTVTED' => ['Agricultural Crops Technology', 'Animal Production'],
         ];
@@ -159,7 +159,7 @@ class StudentController extends Controller
         $results = [];
 
         foreach ($categories as $label => $programs) {
-            $count = Student::whereIn('students_program', $programs)->count();
+            $count = Student::whereIn('students_major', $programs)->count();
             $results[] = [
                 'course' => $label,
                 'count' => $count,
